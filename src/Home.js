e02b34006e03dcbc26cf87badd54c79c4c025d2d
tanverified <khantanveer2u@gmail.com>
2021-03-24 13:54:10 +0530

    title modified

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,9 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        { title: 'Web developer course', body: 'lorem ipsum...', author: 'mario', id: 3 },
+        { title: 'React pro tips', body: 'lorem ipsum...', author: 'mario', id: 4 },
+        { title: 'Redux tips', body: 'lorem ipsum...', author: 'mario', id: 5 }
       ]);
 
     return ( 
