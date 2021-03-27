bd91f0e4f5d596951368e7cc7d701b84e9b19b1b
tanverified <khantanveer2u@gmail.com>
2021-03-27 09:35:13 +0530

    react leeson 13

--- src/Home.js
+++ src/Home.js
@@ -10,10 +10,14 @@ const Home = () => {
         { title: 'Redux tips', body: 'lorem ipsum...', author: 'mario', id: 5 }
       ]);
 
+      const handleDelete = (id) => {
+          const newBlogs = blogs.filter(blog => blog.id !== id);
+          setBlogs(newBlogs);
+      }
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" />
-            <BlogList blogs={blogs.filter( (blog) => blog.author === 'mario' )} title="Mario's Blogs!" />
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            <BlogList blogs={blogs.filter( (blog) => blog.author === 'mario' )} title="Mario's Blogs!" handleDelete={handleDelete} />
         </div>
      );
 }
